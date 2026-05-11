5f6e2f515b3a857f34fedf4fdd18aa29efd2b22c
JFranciosi <luigigigio.jf@gmail.com>
2026-05-11 11:41:27 +0200

    Feat: add styles and routes to navigate between home and profile, add redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { Home } from './components/home/home';
+import { Profile } from './components/profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    {
+        path: 'home',
+        component: Home,
+        title: 'Home'
+    },
+    {
+        path: 'profile',
+        component: Profile,
+        title: 'Profile'
+    },
+    {
+        path: '**',
+        redirectTo: 'home'
+    }
+];
+
+
